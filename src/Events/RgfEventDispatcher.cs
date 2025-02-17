81e6d6cedca7a9b25d6c31c2fc6964fb9684ff14
Tamás Mikola <mikola.tamas@recrovit.hu>
2025-02-17 15:47:45 +0100

    -Improved EventDispatcher to support unsubscribing lambda/anonymous delegates and specific subscribers. -Simplified event raising by introducing RaiseEventAsync method

--- src/Events/RgfEventDispatcher.cs
+++ src/Events/RgfEventDispatcher.cs
@@ -11,44 +11,96 @@ public class RgfEventDispatcher<TEvent, TArgs> where TEvent : notnull where TArg
 
     private EventDispatcher<IRgfEventArgs<TArgs>> _genericHandler = new();
 
-    public void Subscribe(TEvent eventName, Func<IRgfEventArgs<TArgs>, Task> handler, bool defaultHandler = false)
-    {
-        if (handler != null)
-        {
-            EventDispatcher<IRgfEventArgs<TArgs>>? handlers;
-            if (defaultHandler)
-            {
-                if (!_defaultHandlers.TryGetValue(eventName, out handlers))
-                {
-                    handlers = new();
-                    _defaultHandlers.Add(eventName, handlers);
-                }
-            }
-            else
-            {
-                if (!_eventHandlers.TryGetValue(eventName, out handlers))
-                {
-                    handlers = new();
-                    _eventHandlers.Add(eventName, handlers);
-                }
-            }
-            handlers.Subscribe(handler);
-        }
-    }
+    private List<(object Subscriber, object Handler)> _anonymousHandlers = new();
 
-    public void Subscribe(TEvent eventName, Action<IRgfEventArgs<TArgs>> handler)
+    public Func<IRgfEventArgs<TArgs>, Task> Subscribe(TEvent eventName, Func<IRgfEventArgs<TArgs>, Task> handler, object? subscriberTarget = null) => Subscribe(eventName, handler, false, subscriberTarget);
+    public Func<IRgfEventArgs<TArgs>, Task> Subscribe(TEvent eventName, Func<IRgfEventArgs<TArgs>, Task> handler, bool useDefaultHandler, object? subscriberTarget = null)
     {
-        if (handler != null)
+        EventDispatcher<IRgfEventArgs<TArgs>>? handlers;
+        if (useDefaultHandler)
         {
-            EventDispatcher<IRgfEventArgs<TArgs>>? handlers;
+            if (!_defaultHandlers.TryGetValue(eventName, out handlers))
+            {
+                handlers = new();
+                _defaultHandlers[eventName] = handlers;
+            }
+        }
+        else
+        {
             if (!_eventHandlers.TryGetValue(eventName, out handlers))
             {
                 handlers = new();
                 _eventHandlers.Add(eventName, handlers);
             }
-            handlers.Subscribe(handler);
         }
+
+        handlers.Subscribe(handler);
+
+        if (handler.Target == null && subscriberTarget != null)
+        {
+            _anonymousHandlers.Add(new(subscriberTarget, handler));
+        }
+
+        return handler;
     }
+
+    public Action<IRgfEventArgs<TArgs>> Subscribe(TEvent eventName, Action<IRgfEventArgs<TArgs>> handler, object? subscriberTarget = null) => Subscribe(eventName, handler, false, subscriberTarget);
+    public Action<IRgfEventArgs<TArgs>> Subscribe(TEvent eventName, Action<IRgfEventArgs<TArgs>> handler, bool useDefaultHandler, object? subscriberTarget = null)
+    {
+        EventDispatcher<IRgfEventArgs<TArgs>>? handlers;
+        if (useDefaultHandler)
+        {
+            if (!_defaultHandlers.TryGetValue(eventName, out handlers))
+            {
+                handlers = new();
+                _defaultHandlers[eventName] = handlers;
+            }
+        }
+        else
+        {
+            if (!_eventHandlers.TryGetValue(eventName, out handlers))
+            {
+                handlers = new();
+                _eventHandlers.Add(eventName, handlers);
+            }
+        }
+
+        handlers.Subscribe(handler);
+
+        if (handler.Target == null && subscriberTarget != null)
+        {
+            _anonymousHandlers.Add(new(subscriberTarget, handler));
+        }
+
+        return handler;
+    }
+
+    public Func<IRgfEventArgs<TArgs>, Task> Subscribe(TEvent[] eventNames, Func<IRgfEventArgs<TArgs>, Task> handler, object? subscriberTarget = null) => Subscribe(eventNames, handler, false, subscriberTarget);
+    public Func<IRgfEventArgs<TArgs>, Task> Subscribe(TEvent[] eventNames, Func<IRgfEventArgs<TArgs>, Task> handler, bool useDefaultHandler, object? subscriberTarget = null)
+    {
+        Array.ForEach(eventNames, (e) => Subscribe(e, handler, useDefaultHandler, subscriberTarget));
+        return handler;
+    }
+
+    public Action<IRgfEventArgs<TArgs>> Subscribe(TEvent[] eventNames, Action<IRgfEventArgs<TArgs>> handler, object? subscriberTarget = null) => Subscribe(eventNames, handler, false, subscriberTarget);
+    public Action<IRgfEventArgs<TArgs>> Subscribe(TEvent[] eventNames, Action<IRgfEventArgs<TArgs>> handler, bool useDefaultHandler, object? subscriberTarget = null)
+    {
+        Array.ForEach(eventNames, (e) => Subscribe(e, handler, useDefaultHandler, subscriberTarget));
+        return handler;
+    }
+
+    public Func<IRgfEventArgs<TArgs>, Task> Subscribe(Func<IRgfEventArgs<TArgs>, Task> handler, object? subscriberTarget = null)
+    {
+        _genericHandler.Subscribe(handler);
+
+        if (handler.Target == null && subscriberTarget != null)
+        {
+            _anonymousHandlers.Add(new(subscriberTarget, handler));
+        }
+
+        return handler;
+    }
+
 
     public void Unsubscribe(TEvent eventName, Func<IRgfEventArgs<TArgs>, Task> handler)
     {
@@ -80,6 +132,63 @@ public class RgfEventDispatcher<TEvent, TArgs> where TEvent : notnull where TArg
         }
     }
 
+    public void Unsubscribe(Func<IRgfEventArgs<TArgs>, Task> handler)
+    {
+        foreach (var dispatcher in _eventHandlers.Values)
+        {
+            dispatcher.Unsubscribe(handler);
+        }
+        foreach (var dispatcher in _defaultHandlers.Values)
+        {
+            dispatcher.Unsubscribe(handler);
+        }
+        _genericHandler.Unsubscribe(handler);
+    }
+
+    public void Unsubscribe(Action<IRgfEventArgs<TArgs>> handler)
+    {
+        foreach (var dispatcher in _eventHandlers.Values)
+        {
+            dispatcher.Unsubscribe(handler);
+        }
+        foreach (var dispatcher in _defaultHandlers.Values)
+        {
+            dispatcher.Unsubscribe(handler);
+        }
+        _genericHandler.Unsubscribe(handler);
+    }
+
+    public void Unsubscribe(object subscriberTarget)
+    {
+        foreach (var target in _anonymousHandlers.Where((e) => e.Subscriber == subscriberTarget).ToArray())
+        {
+            if (target.Handler is Func<IRgfEventArgs<TArgs>, Task> asyncHandler)
+            {
+                Unsubscribe(asyncHandler);
+            }
+            else if (target.Handler is Action<IRgfEventArgs<TArgs>> handler)
+            {
+                Unsubscribe(handler);
+            }
+            _anonymousHandlers.Remove(target);
+        }
+
+        foreach (var dispatcher in _eventHandlers.Values)
+        {
+            dispatcher.Unsubscribe(subscriberTarget);
+        }
+        foreach (var dispatcher in _defaultHandlers.Values)
+        {
+            dispatcher.Unsubscribe(subscriberTarget);
+        }
+        _genericHandler.Unsubscribe(subscriberTarget);
+    }
+
+    public void Unsubscribe(TEvent[] eventNames, Func<IRgfEventArgs<TArgs>, Task> handler) => Array.ForEach(eventNames, (e) => Unsubscribe(e, handler));
+
+    public void Unsubscribe(TEvent[] eventNames, Action<IRgfEventArgs<TArgs>> handler) => Array.ForEach(eventNames, (e) => Unsubscribe(e, handler));
+
+
     public async Task<bool> DispatchEventAsync(TEvent eventName, IRgfEventArgs<TArgs> args)
     {
         if (_eventHandlers.TryGetValue(eventName, out var handlers))
@@ -96,12 +205,12 @@ public class RgfEventDispatcher<TEvent, TArgs> where TEvent : notnull where TArg
         return args.Handled;
     }
 
-    public void Subscribe(TEvent[] eventNames, Func<IRgfEventArgs<TArgs>, Task> handler, bool defaultHandler = false) => Array.ForEach(eventNames, (e) => Subscribe(e, handler, defaultHandler));
-    public void Subscribe(TEvent[] eventNames, Action<IRgfEventArgs<TArgs>> handler) => Array.ForEach(eventNames, (e) => Subscribe(e, handler));
-    public void Subscribe(Func<IRgfEventArgs<TArgs>, Task> handler) => _genericHandler.Subscribe(handler);
-    public void Unsubscribe(TEvent[] eventNames, Func<IRgfEventArgs<TArgs>, Task> handler) => Array.ForEach(eventNames, (e) => Unsubscribe(e, handler));
-    public void Unsubscribe(TEvent[] eventNames, Action<IRgfEventArgs<TArgs>> handler) => Array.ForEach(eventNames, (e) => Unsubscribe(e, handler));
-    public void Unsubscribe(Func<IRgfEventArgs<TArgs>, Task> handler) => _genericHandler.Unsubscribe(handler);
+    public Task<bool> RaiseEventAsync(TEvent eventKind, object sender)
+    {
+        var eventArgs = Activator.CreateInstance(typeof(TArgs), eventKind);
+        var args = Activator.CreateInstance(typeof(RgfEventArgs<TArgs>), sender, eventArgs);
+        return DispatchEventAsync(eventKind, (IRgfEventArgs<TArgs>)args!);
+    }
 }
 
 public class RgfEventArgs<TArgs> : IRgfEventArgs<TArgs> where TArgs : EventArgs

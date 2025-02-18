7955b81385a887ed39c4eac0a759d8fe459d7bc5
Tamás Mikola <mikola.tamas@recrovit.hu>
2025-02-18 12:27:26 +0100

    Introduced RgfDialog events and related parameters

---
+++ src/Events/RgfRgfDialogEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace Recrovit.RecroGridFramework.Client.Events;
+
+public enum RgfDialogEventKind
+{
+    Initialized = 1,
+    Close = 2,
+    Destroy = 3,
+    Refresh = 4,
+}
+
+public class RgfDialogEventArgs : EventArgs
+{
+    public RgfDialogEventArgs(RgfDialogEventKind eventKind)
+    {
+        EventKind = eventKind;
+    }
+
+    public RgfDialogEventKind EventKind { get; }
+}
